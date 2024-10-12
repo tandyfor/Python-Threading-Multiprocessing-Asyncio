b9ece989ff154738b9228adc4d1e02ba6a0e30eb
Aleksey <alex.mitruagin@gmail.com>
2024-10-12 23:32:24 +0300

    Started Task 2.

---
+++ src/asyncio/task2.py
@@ -0,0 +1,17 @@
+import asyncio
+import os
+import curses
+
+import prettytable
+
+def main():
+    pass
+
+def path_checker():
+    pass
+
+class Downloader():
+    pass
+
+if __name__ == "__main__":
+    pass
