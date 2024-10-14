5fb6e809fa8949a0fbc730dc95da183e8e5079aa
Aleksey <alex.mitruagin@gmail.com>
2024-10-14 22:31:09 +0300

    Simple View.

--- src/asyncio/task2.py
+++ src/asyncio/task2.py
@@ -7,12 +7,19 @@ import aiohttp
 import aiofiles
 import prettytable
 
+SUCCESS = "Успех"
+FAIL = "Ошибка"
 
-class Link():
+class Link(): 
     def __init__(self, link: str):
-        self.link = link
+        self.link = link # TODO: Добавить поиск расширения файла для корректного сохранения.
         self.status = None
 
+    def __str__(self):
+        return f"{self.link} {self.status}"
+
+    def get_row(self):
+        return self.link, self.status
 
 class Downloader():
     def __init__(self, links_list: list[Link], path: str):
@@ -22,14 +29,18 @@ class Downloader():
 
     async def download(self, link: Link):
         async with aiohttp.ClientSession() as session:
-            async with session.get(link.link) as responce:
-                print(f"Responce {responce.status}")
-                # html = await responce.text()
-                # print(f"Body: {html}")
-                f = await aiofiles.open(f'{self.path}/file_{self.n}.jpeg', mode='wb')
-                self.n += 1
-                await f.write(await responce.read())
-                await f.close()
+            try:
+                async with session.get(link.link) as responce:
+                    if responce.ok:
+                        f = await aiofiles.open(f'{self.path}/file_{self.n}.jpeg', mode='wb')
+                        self.n += 1
+                        await f.write(await responce.read())
+                        await f.close()
+                        link.status = SUCCESS
+                    else:
+                        print(responce.status)
+            except:
+                link.status = FAIL
 
     def download_all(self):
         return [self.download(link) for link in self.links_list]
@@ -42,8 +53,14 @@ class Viewer():
         self.links = links_list
 
     def update_links(self):
-        pass
+        self.table.clear_rows()
+        for link in self.links:
+            self.table.add_row(link.get_row())
+        print("\033c")
+        print(self.table)
 
+    def __str__(self):
+        return self.table.get_string()
 
 def path_checker():
     path = ""
@@ -56,13 +73,21 @@ def main():
         Link("https://i.pinimg.com/originals/36/75/27/367527aa5f6f0fd80d86858ae25bb089.jpg"),
         Link("https://mir-s3-cdn-cf.behance.net/projects/original/4a627163531171.Y3JvcCwxMDI3NCw4MDQyLDk2NCww.jpg"),
         Link("https://www.ixpap.com/images/2022/02/Blade-Runner-Wallpaper-13.jpg"),
-        Link("https://i.pinimg.com/originals/2d/a2/5a/2da25a2b249359f26f0335fc164d58c5.jpg")
+        Link("https://i.pinimg.com/originals/2d/a2/5a/2da25a2b249359f26f0335fc164d58c5.jpg"),
+        Link("https://i.pinimg.com/originals/51/3c/86/513c863cee2dfbbd7e61b3f5f2fbeb00.png"),
+        Link("dwa")
         ]
     d = Downloader(links, path_checker())
+    v = Viewer(links)
+    v.update_links()
+    print(v)
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
     loop.run_until_complete(asyncio.gather(*d.download_all()))
     loop.close()
+    v.update_links()
+    print(v)
+
 
 if __name__ == "__main__":
     main()
