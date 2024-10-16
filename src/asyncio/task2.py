302dd6ea2e73d8d19cba7f5bbed11e5d18ff734f
Aleksey <alex.mitruagin@gmail.com>
2024-10-16 18:17:18 +0300

    Async input.

--- src/asyncio/task2.py
+++ src/asyncio/task2.py
@@ -33,6 +33,7 @@ class Downloader():
         self.viewer = Viewer(self.links_list)
 
     async def download(self, link: Link):
+        print(self.viewer)
         async with aiohttp.ClientSession() as session:
             try:
                 async with session.get(link.link) as responce:
@@ -74,19 +75,21 @@ def path_checker():
         path = input("Введите путь для сохранения файлов: ")
     return path
 
+async def async_input(links: list[Link], downloader: Downloader):
+    while True:
+        link = await aioconsole.ainput("Input linl or press Enter to exit:\n")
+        if not link: break
+        link = Link(link)
+        links.append(link)
+        asyncio.create_task(downloader.download(link))
+
+
 def main():
+    links = []
+    d = Downloader(links, path_checker())
     loop = asyncio.new_event_loop()
     asyncio.set_event_loop(loop)
-    links = [
-        Link("https://i.pinimg.com/originals/36/75/27/367527aa5f6f0fd80d86858ae25bb089.jpg"),
-        Link("https://mir-s3-cdn-cf.behance.net/projects/original/4a627163531171.Y3JvcCwxMDI3NCw4MDQyLDk2NCww.jpg"),
-        Link("https://www.ixpap.com/images/2022/02/Blade-Runner-Wallpaper-13.jpg"),
-        Link("https://i.pinimg.com/originals/2d/a2/5a/2da25a2b249359f26f0335fc164d58c5.jpg"),
-        Link("https://i.pinimg.com/originals/51/3c/86/513c863cee2dfbbd7e61b3f5f2fbeb00.png"),
-        Link("dwa")
-        ]
-    d = Downloader(links, path_checker())
-    loop.run_until_complete(asyncio.gather(*d.download_all()))
+    loop.run_until_complete(async_input(links, d))
     loop.close()
 
 
@@ -102,4 +105,9 @@ if __name__ == "__main__":
 # https://www.ixpap.com/images/2022/02/Blade-Runner-Wallpaper-13.jpg
 # https://i.pinimg.com/originals/2d/a2/5a/2da25a2b249359f26f0335fc164d58c5.jpg
 # https://i.pinimg.com/originals/51/3c/86/513c863cee2dfbbd7e61b3f5f2fbeb00.png
-# dwa
+# https://avatars.mds.yandex.net/i?id=1ce48e46d9be18457cc407e0fd3f84db-4969866-images-thumbs&n=13
+# https://avatars.mds.yandex.net/i?id=e81d8ced97fc0b756754d3e698a63f38_l-5281441-images-thumbs&n=13
+# https://avatars.mds.yandex.net/i?id=c35cfcea1dead6bb9760c7cc56fd5057_l-4234782-images-thumbs&n=13
+# https://img.goodfon.com/wallpaper/nbig/d/9a/devushka-aziatka-vzgliad-110.webp
+# https://yt3.ggpht.com/ytc/AKedOLQv3piyJBKo-KMp1UVlw7ameDW6-YTSZQSi4nli=s900-c-k-c0x00ffffff-no-rj
+# https://avatars.mds.yandex.net/i?id=c20fcef0886895fe7a8495c2d1f4d781_l-4569757-images-thumbs&n=13
