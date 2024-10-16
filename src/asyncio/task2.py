8515cee232f05a3351159b56153e5c51b673a1f0
Aleksey <alex.mitruagin@gmail.com>
2024-10-16 23:33:02 +0300

    Add file extension to Link Class.

--- src/asyncio/task2.py
+++ src/asyncio/task2.py
@@ -1,31 +1,32 @@
 import asyncio
 import os
-import curses
-import multiprocessing.dummy
 
-import aiohttp
-import aiofiles
 import aioconsole
+import aiofiles
+import aiohttp
 import prettytable
 
-import time
 
 SUCCESS = "Успех"
 FAIL = "Ошибка"
 IN_PROCESS = "В процессе"
 
+
 class Link(): 
     def __init__(self, link: str):
-        self.link = link # TODO: Добавить поиск расширения файла для корректного сохранения.
+        self.link = link
         self.status = IN_PROCESS
-        self.time = 0
+        self.file_extension = self.get_file_extension()
 
     def __str__(self):
         return f"{self.link} {self.status}"
 
     def get_row(self):
-        # return self.link, self.status
-        return self.link, self.status, round(self.time, 2)
+        return self.link, self.status
+    
+    def get_file_extension(self):
+        return  self.link.split('.')[-1] if self.link.split('.')[-1] in ['png', 'jpg', 'jpeg', 'gif'] else 'jpeg'
+
 
 class Downloader():
     def __init__(self, links_list: list[Link], path: str):
@@ -38,30 +39,24 @@ class Downloader():
         print(self.viewer)
         async with aiohttp.ClientSession() as session:
             try:
-                start = time.time()
                 async with session.get(link.link) as responce:
                     if responce.ok:
-                        f = await aiofiles.open(f'{self.path}/file_{self.n}.jpeg', mode='wb')
+                        f = await aiofiles.open(f'{self.path}/file_{self.n}.{link.get_file_extension()}', mode='wb')
                         self.n += 1
                         await f.write(await responce.read())
                         await f.close()
                         link.status = SUCCESS
-                        link.time = time.time() - start
                     else:
                         print(responce.status)
             except:
                 link.status = FAIL
         print(self.viewer)
 
-    def download_all(self):
-        return [self.download(link) for link in self.links_list if link.status == IN_PROCESS]
-
 
 class Viewer():
     def __init__(self, links_list: list[Link]):
         self.table =  prettytable.PrettyTable()
-        # self.table.field_names = ["Link", "Status"]
-        self.table.field_names = ["Link", "Status", "Download time"]
+        self.table.field_names = ["Link", "Status"]
         self.links = links_list
 
     def update_links(self):
@@ -72,48 +67,34 @@ class Viewer():
     def __str__(self):
         self.update_links()
         print("\033c")
-        return self.table.get_string()
+        return self.table.get_string() + "\nInput link or press Enter to exit:"
 
 def path_checker():
     path = ""
-    while not os.path.isdir(path) or not os.access(path, os.W_OK): # TODO: Добавить функционал создания директории и проверки прав как в примере в документации.
+    while not os.path.isdir(path) or not os.access(path, os.W_OK):
         path = input("Введите путь для сохранения файлов: ")
     return path
 
-async def async_input(links: list[Link], downloader: Downloader, loop: asyncio.new_event_loop):
+async def async_input(links: list[Link], downloader: Downloader):
     while True:
-        link = await aioconsole.ainput("Input linl or press Enter to exit:\n")
+        link = await aioconsole.ainput("Input link or press Enter to exit:\n")
         if not link: break
         link = Link(link)
         links.append(link)
         asyncio.create_task(downloader.download(link))
-    n = 0
     while True:
-        n += 1
         await asyncio.sleep(0.1)
-        tasks = len(asyncio.all_tasks())
-        print(f"{n} {tasks}")
-        if tasks == 1:
-            # loop.stop()
-            return
+        if len(asyncio.all_tasks()) == 1: break
 
 
 def main():
     links = []
     d = Downloader(links, path_checker())
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
-    asyncio.run(async_input(links, d, loop))
-    # loop.run_in_executor(async_input(links, d, loop))
-    # loop.run_forever() # TODO: Добавить функционал для завершения.
-    loop.close()
+    asyncio.run(async_input(links, d))
 
 
 if __name__ == "__main__":
-    s = time.time()
     main()
-    print(time.time() - s)
-
 
 
 # https://i.pinimg.com/originals/36/75/27/367527aa5f6f0fd80d86858ae25bb089.jpg
